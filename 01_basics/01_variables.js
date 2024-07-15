dbbbff25f0aabc7774023ad12d36ca7a90500750
SenaAshu <ashutoshsenapati098@gmail.com>
2024-07-15 18:49:41 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 14453
+let accountEmail = "maxi@gmail.com"
+var accountPassword = "12345"
+accountcity = "Jaipur"
+
+console.log(accountId);
+
+accountEmail = "hc@gmail.com";
+accountPassword = "121212";
+accountcity = "banglore"
+
+let accountstate
+
+console.table([accountEmail,accountPassword,accountcity,accountstate])
