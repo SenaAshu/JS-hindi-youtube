c698b9d81638318bf62bb5727240a60f15f5dbaa
SenaAshu <ashutoshsenapati098@gmail.com>
2024-07-17 19:47:46 +0530

    Arrays_basic

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,36 @@
+// // // // // let myArr = [0,1,2,3,4,5]
+// // // // // console.log(myArr[3]);
+
+// // // // const heros = ['tONY','tHOR']
+// // // // let myArr = [0,1,2,3,4,5]
+// // // // myArr.push(6)
+// // // // console.log(myArr);
+
+// // // // myArr.pop()
+// // // // console.log(myArr);
+
+// // // let myArr = [0,1,2,3,4,5]
+// // // myArr.unshift(9)
+// // // console.log(myArr);
+
+// // // myArr.shift()
+// // // console.log(myArr);
+
+// // // myArr.shift()
+// // // console.log(myArr);
+
+// // // let myArr = [0,1,2,3,4,5]
+// // // console.log(myArr.includes(5));
+// // // console.log(myArr.indexOf(19));
+// // // console.log(myArr.indexOf(11));
+
+// // let myArr = [0,1,2,3,4,5]
+// // console.log(typeof (myArr.join()));   
+// // console.log(myArr);
+
+
+// let myArr = [0,1,2,3,4,5]
+// console.log(myArr.slice(1,3));
+
+// console.log(myArr.splice(1,3));
+// console.log(myArr);
