26e496f0c0f0a9887949122b1d3cb4543e827786
SenaAshu <ashutoshsenapati098@gmail.com>
2024-07-19 15:54:49 +0530

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,11 +1,45 @@
-let a =50          // GLOBAL SCOPE
-var c = 100         // GLOBAL SCOPE
-if(true){          
-    let a = 10     //
-    const b = 23   //       BLOCK SCOPE
-    var c = 30     //
-}
+// // // // // let a =50          // GLOBAL SCOPE
+// // // // // var c = 100         // GLOBAL SCOPE
+// // // // // if(true){          
+// // // // //     let a = 10     //
+// // // // //     const b = 23   //       BLOCK SCOPE
+// // // // //     var c = 30     //
+// // // // // }
 
-console.log(a);
-// console.log(b);
-console.log(c);
+// // // // // console.log(a);
+// // // // // // console.log(b);
+// // // // // console.log(c);
+
+// // // // const user = {
+// // // //     username:"Maxi",
+// // // //     price:999,
+
+// // // //     objectkey:function(){
+// // // //         console.log(`${this.username},Welcome!`);
+// // // //     }
+// // // // }
+// // // // user.objectkey()
+
+// // // let name = function(num1,num2){
+// //     // return num1+num2
+// // // }
+
+
+// // let sum=(num1,num2) => num1+num2
+// // console.log("Executed");
+
+// // // console.log(sum(2,5));
+
+// // // let sum2=(num1,num2) => ({name:"Maxi",
+// // //     roll:198
+// // // })
+// // // console.log(sum(2,5));
+// // let num1 = 12
+// // let num2 =18
+// // if(num1>num2)
+// //     console.log("Num1 is greater");
+// // else console.log("Num2");
+
+
+// let sum = (num) => {return num+1}
+// console.log(sum(2)); 
---
+++ 03_basics/03_arrows.js
@@ -0,0 +1,17 @@
+// const user= {
+//     name:"maxi",
+//     roll:198,
+//     object:function(){
+//         console.log(`${this.name}, Welcome!`);
+//     }
+   
+// }
+// user.object()
+
+let username = (num1,num2) => {
+    return num1+num2
+}
+// console.log(username(4,4));
+
+let username2 = (num1,num2) => (num1+num2)
+console.log(username2(9,9));
