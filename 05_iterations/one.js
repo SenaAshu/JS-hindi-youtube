caa63fadb91c001bca9c2f34cdfc040a2a7bf6fc
SenaAshu <ashutoshsenapati098@gmail.com>
2024-07-21 14:34:35 +0530

    loops1

---
+++ 05_iterations/one.js
@@ -0,0 +1,37 @@
+// // // // // for loop
+// // // //  for(let i=0;i<10;i++){
+// // // //     console.log(i);
+// // // //     if(i==5)console.log('5 founded!');
+// // // //  }
+
+// // // // //  for (let index = 0; index < array.length; index++) {
+// // // // //     const element = array[index];
+    
+// // // // //  }
+
+// // // for (let i = 0; i < 10; i++) {
+// // //     console.log(`MULTIPLICATION VALUES OF ${i}`);
+// // //     for(let j=0;j<=10;j++){
+// // //         console.log(`${i} * ${j} = `+i*j);
+// // //     }
+    
+// // // }
+
+// // let myarray = ["Maxi","Soumya","Kamal"]
+// // for(let i=0;i<myarray.length;i++){
+// //     console.log(myarray[i]);
+// // }
+
+// for(let i=1;i<=20;i++){
+//     if(i==7){
+//         continue
+//     }
+//     console.log(i);
+// }
+
+// for(let i=1;i<=20;i++){
+//     if(i==7){
+//         break
+//     }
+//     console.log(i);
+// }
