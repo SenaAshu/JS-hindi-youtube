aad68a6e4bb1e8037cbc9f137fab1b30970f8eef
SenaAshu <ashutoshsenapati098@gmail.com>
2024-07-23 18:14:20 +0530

    new

--- 30 days challenge/task-5.js
+++ 30 days challenge/task-5.js
@@ -1,147 +1,183 @@
-// // // // // // // // // // // // // // ///////////////////// ACTIVITY 1
-// // // // // // // // // // // // // // function evenorodd(num){
-// // // // // // // // // // // // // //     let result = (num%2)==0?"Even":"Odd"
-// // // // // // // // // // // // // //     return console.log(result);
+// // // // // // // // // // // // // // // // ///////////////////// ACTIVITY 1
+// // // // // // // // // // // // // // // // function evenorodd(num){
+// // // // // // // // // // // // // // // //     let result = (num%2)==0?"Even":"Odd"
+// // // // // // // // // // // // // // // //     return console.log(result);
+// // // // // // // // // // // // // // // // }
+// // // // // // // // // // // // // // // // evenorodd(14)
+
+// // // // // // // // // // // // // // // function power(num){
+// // // // // // // // // // // // // // //     let result = Math.pow(num,2)
+// // // // // // // // // // // // // // //     return result
+// // // // // // // // // // // // // // // }
+// // // // // // // // // // // // // // // console.log(power(10));
+
+// // // // // // // // // // // // // // //                   ACTIVITY 2
+
+// // // // // // // // // // // // // // // function greater(num1,num2){
+// // // // // // // // // // // // // // //      return num1>num2?`${num1} is greater`:`${num2} is greater`
+// // // // // // // // // // // // // // // }
+// // // // // // // // // // // // // // // console.log(greater(17,9))
+
+// // // // // // // // // // // // // // function concat(str1,str2){
+// // // // // // // // // // // // // //     str3 = str1.concat(" "+str2)
+// // // // // // // // // // // // // //     return str3
 // // // // // // // // // // // // // // }
-// // // // // // // // // // // // // // evenorodd(14)
+// // // // // // // // // // // // // // console.log(concat("Hello","World"));
 
-// // // // // // // // // // // // // function power(num){
-// // // // // // // // // // // // //     let result = Math.pow(num,2)
-// // // // // // // // // // // // //     return result
+
+
+// // // // // // // // // // // // // function evenOdd(num){
+// // // // // // // // // // // // //     if(num%2==0){
+// // // // // // // // // // // // //         console.log("Even");
+// // // // // // // // // // // // //     }else{
+// // // // // // // // // // // // //         console.log("odd");
+// // // // // // // // // // // // //     }
 // // // // // // // // // // // // // }
-// // // // // // // // // // // // // console.log(power(10));
+// // // // // // // // // // // // // evenOdd(3)
 
-// // // // // // // // // // // // //                   ACTIVITY 2
-
-// // // // // // // // // // // // // function greater(num1,num2){
-// // // // // // // // // // // // //      return num1>num2?`${num1} is greater`:`${num2} is greater`
-// // // // // // // // // // // // // }
-// // // // // // // // // // // // // console.log(greater(17,9))
-
-// // // // // // // // // // // // function concat(str1,str2){
-// // // // // // // // // // // //     str3 = str1.concat(" "+str2)
-// // // // // // // // // // // //     return str3
+// // // // // // // // // // // // function square(num){
+// // // // // // // // // // // //     return Math.pow(num,2)
 // // // // // // // // // // // // }
-// // // // // // // // // // // // console.log(concat("Hello","World"));
+// // // // // // // // // // // // console.log(square(5));
 
 
+// // // // // // // // // // // const result = ((num)=>Math.pow(num,2))
+// // // // // // // // // // // console.log(result(2));
 
-// // // // // // // // // // // function evenOdd(num){
-// // // // // // // // // // //     if(num%2==0){
-// // // // // // // // // // //         console.log("Even");
-// // // // // // // // // // //     }else{
-// // // // // // // // // // //         console.log("odd");
-// // // // // // // // // // //     }
-// // // // // // // // // // // }
-// // // // // // // // // // // evenOdd(3)
-
-// // // // // // // // // // function square(num){
-// // // // // // // // // //     return Math.pow(num,2)
+// // // // // // // // // // function concatenation(str1,str2,str4){
+// // // // // // // // // //     let str3 = (str1+" "+str2+" "+str4)
+// // // // // // // // // //     console.log(str3);
 // // // // // // // // // // }
-// // // // // // // // // // console.log(square(5));
+// // // // // // // // // // concatenation("Hello","World","Welcome")
 
+// // // // // // // // // // let newstring = ((str1,str2)=>(str1+" "+str2))
+// // // // // // // // // // console.log(newstring("Hello","World"));
 
-// // // // // // // // // const result = ((num)=>Math.pow(num,2))
-// // // // // // // // // console.log(result(2));
+// // // // // // // // // function characterCheck(str,chek){
+// // // // // // // // //     if(str.includes(chek)){
+// // // // // // // // //         return true
+// // // // // // // // //     }else{
+// // // // // // // // //         return false
+// // // // // // // // //     }
+// // // // // // // // // }
+// // // // // // // // // // console.log(characterCheck("Heloo","o"));
+// // // // // // // // // // console.log(characterCheck("heloo","z"));
 
-// // // // // // // // function concatenation(str1,str2,str4){
-// // // // // // // //     let str3 = (str1+" "+str2+" "+str4)
-// // // // // // // //     console.log(str3);
+// // // // // // // // // const CharacterCheck = (str,chek)=>str.includes(chek)
+// // // // // // // // // console.log(characterCheck("Hello","H"));
+// // // // // // // // // console.log(characterCheck("Hello","z"));
+
+// // // // // // // // function product(para1,para2=10){
+// // // // // // // //     return para1*para2
 // // // // // // // // }
-// // // // // // // // concatenation("Hello","World","Welcome")
+// // // // // // // // // console.log(product(2));
 
-// // // // // // // // let newstring = ((str1,str2)=>(str1+" "+str2))
-// // // // // // // // console.log(newstring("Hello","World"));
+// // // // // // // // let count = (n1,n2=2)=>(n1*n2)
+// // // // // // // // console.log(count(3  ));
 
-// // // // // // // function characterCheck(str,chek){
-// // // // // // //     if(str.includes(chek)){
-// // // // // // //         return true
-// // // // // // //     }else{
-// // // // // // //         return false
-// // // // // // //     }
+// // // // // // // function info(name,age=20){
+// // // // // // //     // console.log(`Greeting! ${name}, Welcome On board and your age is ${age}`);
 // // // // // // // }
-// // // // // // // // console.log(characterCheck("Heloo","o"));
-// // // // // // // // console.log(characterCheck("heloo","z"));
+// // // // // // // info("maxi");
 
-// // // // // // // const CharacterCheck = (str,chek)=>str.includes(chek)
-// // // // // // // console.log(characterCheck("Hello","H"));
-// // // // // // // console.log(characterCheck("Hello","z"));
+// // // // // // // let info2 =(name,age=20) => (console.log(`Greeting! ${name}, Welcome On board and your age is ${age}`))
+// // // // // // // info2("maxi",22)
 
-// // // // // // function product(para1,para2=10){
-// // // // // //     return para1*para2
+// // // // // // // Higher-order function
+// // // // // // function repeatFunction(fn, times) {
+// // // // // //     for (let i = 0; i < times; i++) {
+// // // // // //         fn();
+// // // // // //     }
 // // // // // // }
-// // // // // // // console.log(product(2));
 
-// // // // // // let count = (n1,n2=2)=>(n1*n2)
-// // // // // // console.log(count(3  ));
+// // // // // // // Example function to print a message
+// // // // // // function sayHello() {
+// // // // // //     console.log("Hello!");
+// // // // // // }
 
-// // // // // function info(name,age=20){
-// // // // //     // console.log(`Greeting! ${name}, Welcome On board and your age is ${age}`);
+// // // // // // // Another example function to print a different message
+// // // // // // function sayGoodbye() {
+// // // // // //     console.log("Goodbye!");
+// // // // // // }
+
+// // // // // // // Example usage
+// // // // // // repeatFunction(sayHello, 3);  // Output: "Hello!" (printed 3 times)
+// // // // // // repeatFunction(sayGoodbye, 2); // Output: "Goodbye!" (printed 2 times)
+
+
+
+// // // // // function main2(fn,number){
+// // // // //     for(let i=1;i<=number;i++){
+// // // // //         fn()
+// // // // //     }
 // // // // // }
-// // // // // info("maxi");
 
-// // // // // let info2 =(name,age=20) => (console.log(`Greeting! ${name}, Welcome On board and your age is ${age}`))
-// // // // // info2("maxi",22)
+// // // // // function hello(){
+// // // // //     console.log("Hello");
+// // // // // }
 
-// // // // // Higher-order function
-// // // // function repeatFunction(fn, times) {
-// // // //     for (let i = 0; i < times; i++) {
-// // // //         fn();
+// // // // // function hiii(){
+// // // // //     console.log("Hiiiii");
+// // // // // }
+
+// // // // // main2(hello,3);
+// // // // // main2(hiii,6);
+
+
+// // // // function lastwork(fn,times){
+// // // //     for(let i=0;i<times;i++){
+// // // //         fn()
 // // // //     }
 // // // // }
 
-// // // // // Example function to print a message
-// // // // function sayHello() {
-// // // //     console.log("Hello!");
+// // // // function sayhello(){
+// // // //     console.log("Hello");
 // // // // }
 
-// // // // // Another example function to print a different message
-// // // // function sayGoodbye() {
-// // // //     console.log("Goodbye!");
+// // // // function sayhii(){
+// // // //     console.log("Hii");
 // // // // }
 
-// // // // // Example usage
-// // // // repeatFunction(sayHello, 3);  // Output: "Hello!" (printed 3 times)
-// // // // repeatFunction(sayGoodbye, 2); // Output: "Goodbye!" (printed 2 times)
+// // // // lastwork(sayhello,3)
+// // // // lastwork(sayhii,2)
+
+// // // function firstone
 
 
-
-// // // function main2(fn,number){
-// // //     for(let i=1;i<=number;i++){
-// // //         fn()
-// // //     }
-// // // }
-
-// // // function hello(){
-// // //     console.log("Hello");
-// // // }
-
-// // // function hiii(){
-// // //     console.log("Hiiiii");
-// // // }
-
-// // // main2(hello,3);
-// // // main2(hiii,6);
-
-
-// // function lastwork(fn,times){
-// //     for(let i=0;i<times;i++){
-// //         fn()
-// //     }
+// // function first(fn1,fn2,val){
+// //     let result1 = fn1(val)
+// //     let final = fn2(result1)
+// //     console.log(final);
 // // }
 
-// // function sayhello(){
-// //     console.log("Hello");
+// // function double(val){
+// //     return val*2
 // // }
 
-// // function sayhii(){
-// //     console.log("Hii");
+// // function square(val){
+// //     return val*val
 // // }
 
-// // lastwork(sayhello,3)
-// // lastwork(sayhii,2)
+// // first(double,square,5)
 
-// function firstone
+// function lastwork(fn,times){
+//         for(let i=0;i<times;i++){
+//             fn()
+//         }
+//     }
+    
+//     function sayhello(){
+//         console.log("Hello");
+//     }
+    
+//     function sayhii(){
+//         console.log("Hii");
+//     }
+    
+//     lastwork(sayhello,3)
+//     lastwork(sayhii,2)
+
+
 
 
 function first(fn1,fn2,val){
---
+++ 30 days challenge/task-6.js
@@ -0,0 +1,46 @@
+// const arr = [1,2,3,4,5]
+// console.log(arr[3]);
+// console.log(arr[0],arr[arr.length-1]);
+
+// arr.push(11)
+// console.log(arr);
+
+// arr.pop()
+// console.log(arr);
+
+// arr.shift()
+// console.log(arr);
+
+// arr.unshift(9)
+// console.log(arr);
+
+// const newarr = arr.map((key)=>(key*2))
+// console.log(arr);
+// console.log(newarr);
+
+// const newarr = arr.filter((key)=>(key>3))
+// console.log(newarr);
+
+// let newArr = arr.reduce((acc,curr)=>{
+//     // console.log(`accumalator is:${acc}     current is:${curr}`);
+//     return acc+curr
+
+// },0)
+// console.log(newArr);
+
+
+const arr = [1,2,3,4,5]
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+// const newArr = arr.forEach((key)=>(console.log(key)))
+
+const matrix = [ [1,2,3], [4,5,6], [7,8,9]]
+// for(let i=0;i<matrix.length;i++){
+//     for(let j=0;j<matrix[i].length;j++){
+//         console.log(`Value:${matrix[i][j]}    index: ${ i } ${j}`);
+//     }
+// }
+console.log(matrix[1][2]);
+console.log(matrix[2][1]);
