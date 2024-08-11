2364dd5ad88ce95fef4735dd085131f000a6416d
SenaAshu <ashutoshsenapati098@gmail.com>
2024-08-11 19:54:09 +0530

    randoms

---
+++ 10_Classes&OOP/oop.js
@@ -0,0 +1,13 @@
+const user = {
+    username: "Maxwel",
+    id:198,
+    location:"Banglore",
+
+    getUserDetails :function(){
+        console.log("Data has been collected.");
+        
+    }
+}
+console.log(user.username);
+console.log(user.getUserDetails());
+
